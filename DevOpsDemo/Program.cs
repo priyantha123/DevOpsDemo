129fd914c690f2f94107edddea6962536cb174e9
Weerasingha Dinesh Priyantha <d.priyantha.w@enexcosmh.onmicrosoft.com>
2021-02-16 17:48:26 +0900

    Add project files.

---
+++ DevOpsDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevOpsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
