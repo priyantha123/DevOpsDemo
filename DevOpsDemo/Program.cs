dfc919c7b5c60a565cb3634dcbccbc5379f1037f
Weerasingha Dinesh Priyantha <d.priyantha.w@enexcosmh.onmicrosoft.com>
2021-02-17 21:55:37 +0900

    Singleto class updates

--- DevOpsDemo/Program.cs
+++ DevOpsDemo/Program.cs
@@ -7,25 +7,34 @@ namespace DevOpsDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Singleton singleton = Singleton.GetInstance();
+            singleton.LoginId = "Dinesh";
+            singleton.RoleId = "Admin";
+
         }        
     }
 
     public class Singleton
     {
         private static Singleton instance = null;
+        public string LoginId { get; set; }
+        public string RoleId { get; set; }
         private Singleton()
         {
 
         }
 
-        public static Singleton GetInstance
+        private static object lockThis = new object();
+
+        public static Singleton GetInstance()
         {
-            get
-            {
-                if (instance == null)
+            lock(lockThis){
+                if (Singleton.instance == null)
                     instance = new Singleton();
-                return instance;
             }
+
+            return instance;
         }
     }
 }
